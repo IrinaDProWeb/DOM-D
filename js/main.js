b2ef41c53b706647af5ecf9795cb5ee976505bb6
IrinaDProWeb <98805290+IrinaDProWeb@users.noreply.github.com>
2022-05-16 21:30:28 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ $(function () {
   $('.slider__box').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: "<img src='../images/prev-arrow.svg' class='prev' alt='1'>",
-    nextArrow: "<img src='../images/next-arrow.svg' class='next' alt='2'>",
+    prevArrow: "<img src='./images/prev-arrow.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='./images/next-arrow.svg' class='next' alt='2'>",
     responsive: [
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          prevArrow: "<img src='../images/prev-arrow.svg' class='prev' alt='1'>",
-          nextArrow: "<img src='../images/next-arrow.svg' class='next' alt='2'>",
+          prevArrow: "<img src='./images/prev-arrow.svg' class='prev' alt='1'>",
+          nextArrow: "<img src='./images/next-arrow.svg' class='next' alt='2'>",
         }
       }
     ]
