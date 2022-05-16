6b4d7e6b5141774cb71d88ec817db1927d0036a0
IrinaDProWeb <98805290+IrinaDProWeb@users.noreply.github.com>
2022-05-16 20:03:19 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+  $('.slider__box').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: "<img src='../images/prev-arrow.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='../images/next-arrow.svg' class='next' alt='2'>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          prevArrow: "<img src='../images/prev-arrow.svg' class='prev' alt='1'>",
+          nextArrow: "<img src='../images/next-arrow.svg' class='next' alt='2'>",
+        }
+      }
+    ]
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.burger-menu').removeClass('burger-menu--close');
+  });
+
+  $('.burger-menu__btn-close').on('click', function () {
+    $('.burger-menu').addClass('burger-menu--close');
+  });
+
+});
